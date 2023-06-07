bbf06a50e74e9dadda82f548759d7189bdc51004
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-06-07 10:29:49 +0200

    views + seed ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_080221) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_080543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -21,6 +37,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_080221) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Item.destroy_all
+Order.destroy_all
+OrderItem.destroy_all
+CartItem.destroy_all
+
+
+10.times do
+  Item.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: "%.2f"%rand(0.0..100.0),
+    image_url: "https://loremflickr.com/#{rand(290..300)}/#{rand(290..300)}/kitten"
+  )
+end
+
+
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  Cart.create(user_id: user.id)
+end
+
+
+
+Cart.all.each do |cart|
+  rand(1..5).times do
+    item = Item.all.sample
+    CartItem.create(cart_id: cart.id, item_id: item.id)
+  end
+end
+
+
+10.times do
+  user = User.all.sample
+  order = Order.create(user_id: user.id)
+  rand(1..5).times do
+    item = Item.all.sample
+    OrderItem.create(order_id: order.id, item_id: item.id)
+  end
+end
