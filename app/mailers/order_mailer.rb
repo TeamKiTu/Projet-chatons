101541c65b40864de927e6238efd0ab3706a9e99
Guillaume Regimbeau <gregimbeau@mail.com>
2023-06-07 14:24:26 +0200

    mailer new password and order ok

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,22 +1,13 @@
-class UserMailer < ApplicationMailer
+class OrderMailer < ApplicationMailer
   default from: "gregimbeau@gmail.com"
 
-  def welcome(user)
+  def confirmation_order(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url = "http://monsite.fr/login"
      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: "Bienvenue chez nous !")
-  end
-
-  def password_changed(user)
-    @user = user
-    @url = "http://monsite.fr/login"
-    mail(to: @user.email, subject: 'Votre mot de passe a été mis à jour')
+    mail(to: "adminpcthp@yopmail.com", subject: "Bienvenue chez nous !")
   end
 end
-
-
-
-
